642453d509ae02f055356f5446f1c6edd9063668
Derek Tong <dtong8@fordham.edu>
2023-04-18 19:28:16 -0400

    deploy.sh removes config.json from git tracking

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # Give admin permissions to the script:
 # chmod +x deploy.sh
 
+git update-index --skip-worktree config.json # prevents config.json from being commited.
 node deploy-commands.js
 node index.js
