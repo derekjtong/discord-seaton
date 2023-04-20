fa1d1e6387ab114897156e39f6f4adf371148f75
Derek Tong <dtong8@fordham.edu>
2023-04-19 21:52:26 -0400

    Removed dotenv

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # Give admin permissions to the script:
 # chmod +x deploy.sh
 
-git update-index --skip-worktree config.json # prevents config.json from being commited.
 node deploy-commands.js
 node index.js
